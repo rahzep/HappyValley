b2b6ae4a67ab2ab0a61a4cb32dddc7edd6c63f49
Joey Darko <joey.d.darko@gmail.com>
2015-12-24 13:46:19 +0800

    adjust structure of versionable concern

--- app/models/concerns/versionable.rb
+++ app/models/concerns/versionable.rb
@@ -7,27 +7,27 @@ module Versionable
     after_create  :record_create
     before_update :record_update
     before_destroy :record_destroy
-  end
 
-  private
+    private
 
-  def record_create
-    v = self.versions.new
-    v.operation = "create"
-    v.save
-  end
+    def record_create
+      v = self.versions.new
+      v.operation = "create"
+      v.save
+    end
 
-  def record_update
-    v = self.versions.new
-    v.operation = "update"
-    v.object = self.class.find(self.id)# use old record data
-    v.save
-  end
+    def record_update
+      v = self.versions.new
+      v.operation = "update"
+      v.object = self.class.find(self.id)# use old record data
+      v.save
+    end
 
-  def record_destroy
-    v = self.versions.new
-    v.operation = "destroy"
-    v.object = self
-    v.save
+    def record_destroy
+      v = self.versions.new
+      v.operation = "destroy"
+      v.object = self
+      v.save
+    end
   end
 end
